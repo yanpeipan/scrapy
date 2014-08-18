449d8ec39657a359c11f17ba64666edfd0d4e8af
yan <yanpeipan_82@qq.com>
2014-08-18 08:54:53 +0800

    youku

--- Scrapy/spiders/youku.py
+++ Scrapy/spiders/youku.py
@@ -15,24 +15,26 @@ from datetime import datetime, date, time
 from selenium import webdriver
 
 class YoukuSpider(CrawlSpider):
-  name = 'youku'
-  pipelines = []
-  allowed_domins = ['http://www.youku.com', 'https://api.douban.com', 'https://openapi.youku.com']
-  start_urls = ['http://www.youku.com/v_showlist/c0.html']
 
-  def __init__(self, category = None, *args, **kwargs):
-      if category:
-          self.category=category
+    name='youku'
+    allowed_domins=['http://www.youku.com', 'https://openapi.youku.com']
+    start_urls=['http://www.youku.com/v_showlist/c0.html']
 
-  def start_requests(self):
+
+    def __init__(self, category = None, *args, **kwargs):
+        if category:
+            self.category=category
+
+    def start_requests(self):
       return [FormRequest('https://openapi.youku.com/v2/shows/by_category.json', formdata={'client_id':'696c961ded023528', 'category':self.category}, callback=self.parse)]
 
-  def parseCategory(self, response):
+    def parseCategory(self, response):
       pass
 
-  def parse(self, response):
+    def parse(self, response):
       showItem=ShowItem()
       shows=json.loads(response.body)
       for show in shows['shows']:
           showItem['id']=show['id']
           yield showItem
+
